f15fd76a4e05150d5ebb51ca0cc5458eb53b1d19
aarushi91 <aarushijain80911@gmail.com>
2026-07-22 18:21:57 +0530

    Setup Express server and MongoDB Atlas connection

---
+++ server/src/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("🚀 SocialFeed Backend is Running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
+});
